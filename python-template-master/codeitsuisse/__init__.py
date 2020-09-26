6748f0e4606a7536f9d415e8f489788db74a9cf1
GarySGX <shenguangxu1009@gmail.com>
2020-09-26 15:13:06 +0800

    salad update

--- python-template-master/codeitsuisse/__init__.py
+++ python-template-master/codeitsuisse/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask;
 app = Flask(__name__)
-#import codeitsuisse.routes.square
-#import codeitsuisse.routes.contact_trace
-import codeitsuisse.routes.revisitgeometry
-#import codeitsuisse.routes.dummy
-
-
+# import codeitsuisse.routes.square
+# #import codeitsuisse.routes.contact_trace
+# import codeitsuisse.routes.revisitgeometry
+import codeitsuisse.routes.dummy
+import codeitsuisse.routes.salad
 
